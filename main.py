1c55f6823133808fcc27ad7591c29c944cc15151
sajjad <sajjadghan1382@gmail.com>
2022-06-23 17:06:15 +0430

    bug fixed

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ while 9 < len_of_pass < 0:
     len_of_pass = int(input("You should Enter a number between 1 and 9: "))
 
 password = []
-seed(1)
 while len(password) < len_of_pass:
     rand_num = randint(0, 9)
     if rand_num not in password:
